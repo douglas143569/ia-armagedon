acce205b92dac7f6afd0a4f1a301d588d4d9d2f7
Douglas <gabrielvanzella1@gmail.com>
2026-08-27 12:45:25 -0300

    Interface gráfica customizável: HTML/CSS/JS com design profissional

---
+++ abrir-interface.bat
@@ -0,0 +1,18 @@
+@echo off
+REM ARMAGEDON - Interface Grafica Customizavel
+REM Criado em 2026-08-27
+
+echo.
+echo ================================
+echo  ARMAGEDON - Interface Grafica
+echo ================================
+echo.
+
+REM Abrir arquivo HTML no navegador
+start "" "c:\Users\7700781010\Desktop\iadouglas\ia-douglas\interface.html"
+
+echo Interface aberta no navegador!
+echo Mantenha o Ollama rodando em outro terminal.
+echo.
+
+pause
